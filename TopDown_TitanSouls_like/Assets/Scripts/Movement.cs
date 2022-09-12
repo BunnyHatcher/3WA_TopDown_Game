31f100ea80c4b4410464efcb54ebb5881b92e49c
Martin Engelhaus <martinengelhaus@gmx.de>
2022-09-12 15:17:12 +0200

    connected run and idle animations to code

--- TopDown_TitanSouls_like/Assets/Scripts/Movement.cs
+++ TopDown_TitanSouls_like/Assets/Scripts/Movement.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //Components to be referenced
     private Rigidbody2D _rb2d;
     private Animator _animator;
+    
+    //Vectors for movement and orientation
     private Vector2 _direction;
+    private Vector2 _orientation;
 
-   [SerializeField] private float _moveSpeed = 10f;
+    [SerializeField] private float _moveSpeed = 10f;
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         // set up Rigidbody for use in the script
@@ -20,19 +24,36 @@ public class Movement : MonoBehaviour
         _animator = GetComponentInChildren<Animator>();
     }
 
-    
+
     void Update() //receive input for movement
     {
         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        _animator.SetFloat("SpeedX", _direction.x);
-        _animator.SetFloat("SpeedY", _rb2d.velocity.y);
+        // check with magnitude (=length of vector3), if player is moving or not
+        if (_direction.magnitude > 0) //if magnitude is above 0, the player is moving
+        {
+            // use animators for the corresponding movement direction
+            _animator.SetFloat("SpeedX", _direction.x);
+            _animator.SetFloat("SpeedY", _direction.y);
+            _animator.SetBool("isMoving", true);
+
+            // clamp
+            _orientation = Vector2.ClampMagnitude(_direction * 10000f, 1f);
+        }
+
+        else //if magnitude is below 0, the player is not moving
+        {
+            // use animators for the corresponding idle direction
+            _animator.SetFloat("SpeedX", _orientation.x);
+            _animator.SetFloat("SpeedY", _orientation.y);
+            _animator.SetBool("isMoving", false);
+
+        }
 
     }
+        private void FixedUpdate() //transform movement input into actual movement
+        {
+            _rb2d.velocity = _direction.normalized * _moveSpeed;
 
-    private void FixedUpdate() //transform movement input into actual movement
-    {
-        _rb2d.velocity = _direction.normalized * _moveSpeed;
-
-    }
+        }
 }
