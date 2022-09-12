ae476f2e1882b505aab6cb6c0c63e11227e2b3f0
Martin Engelhaus <martinengelhaus@gmx.de>
2022-09-12 12:50:36 +0200

    Worked on movement script, WIP

--- TopDown_TitanSouls_like/Assets/Scripts/Movement.cs
+++ TopDown_TitanSouls_like/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
+    private Animator _animator;
     private Vector2 _direction;
 
    [SerializeField] private float _moveSpeed = 10f;
@@ -13,14 +14,20 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // set up Rigidbody for use in the script
         _rb2d = GetComponent<Rigidbody2D>();
+        // get the Animator as a child of the PlayerRig where we planted our script
+        _animator = GetComponentInChildren<Animator>();
     }
 
     
     void Update() //receive input for movement
     {
         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-                
+
+        _animator.SetFloat("SpeedX", _direction.x);
+        _animator.SetFloat("SpeedY", _rb2d.velocity.y);
+
     }
 
     private void FixedUpdate() //transform movement input into actual movement
